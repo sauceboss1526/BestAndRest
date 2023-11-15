564f166436907c5d06c8c6a50143b214b38d1ca5
sauceboss1526 <97584633+sauceboss1526@users.noreply.github.com>
2023-11-15 04:43:14 +0000

    Introduction first draft

---
+++ 1.0.py
@@ -0,0 +1,13 @@
+""" 
+Alexis Lafraniere of the New York Rangers regarded as a "bust" over the open internet... mainly on X.
+A bust is a player drafted first overall and spends majority of his career playing outside of the NHL (change wording). 
+ First overall pick data will be gathered from NHL statistics websites via BeautifulSoup to compare first overall NHL picks 
+
+ Two datasets will be joined via player name:
+
+ Rookiesdata -->  list of rookies amd their respective draft information.
+                    -Draft Information includes:
+                    -Team: string. The team that selected the player in NHL draft 
+                    -Year: integer. Year player was selected in NHL draft
+                    
+"""
