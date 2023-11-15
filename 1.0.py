20a3ef77a0236d2fd2285ef3e7468bdea713ced6
sauceboss1526 <97584633+sauceboss1526@users.noreply.github.com>
2023-11-15 06:28:35 +0000

    1.1 changes to add more context

--- 1.0.py
+++ 1.0.py
@@ -1,13 +1,45 @@
-""" 
-Alexis Lafraniere of the New York Rangers regarded as a "bust" over the open internet... mainly on X.
+""" Infrences and insight from NHL first round picks and their data on the open internet.
+
+Alexis Lafraniere of the New York Rangers is occasionally labeled as a "bust" through media outlets.  
 A bust is a player drafted first overall and spends majority of his career playing outside of the NHL (change wording). 
- First overall pick data will be gathered from NHL statistics websites via BeautifulSoup to compare first overall NHL picks 
+First overall pick data will be gathered from NHL statistics websites via BeautifulSoup to compare first overall NHL picks.
+This analysis explores relationships among features related to the player's performance  
 
- Two datasets will be joined via player name:
+ Player statistics from obtained datasets is aggregated in Excel Workbooks and then converted to csv files. 
+ -Sample only includes players who have played at least two seasons in the NHL.
+ 
 
- Rookiesdata -->  list of rookies amd their respective draft information.
+ Rookiesdata -->  List of rookies amd their respective draft information.
                     -Draft Information includes:
-                    -Team: string. The team that selected the player in NHL draft 
-                    -Year: integer. Year player was selected in NHL draft
+                    -teamcity: Location in which NHL team is based in. Datatype: string
+                    -teamname: The team that selected the player in NHL draft. Datatype: string. 
+                    -year: Year player was selected in NHL draft. Datatype: integer.
+                    -GP: Number of games player played in the NHL. Datatype: integer.
+                    -PPG: Sum of goals and assists divided by number of games played. Datatype: float.
+                    -G: Total goals scored by player. Datatype: integer.
+                    -A: Total goals assisted by player. Datatype: integer.
+                    -champion: Boolean indicator if player is a Stanley Cup champion. 1 = player has won Stanley Cup in his career 0 = player never won the Stanley Cup. Datatype: integer                
+
+                    returns: 
+
+                    shapeinfo: size of dataframe
+                    nullscounts: count of null values in dataset 
+                    describedf: statistical summary of numerical features 
                     
 """
+import pandas as pd
+import numpy as np 
+import bs4 
+from bs4 import BeautifulSoup
+
+class scrape:
+    def __init__(self, htmlscript):
+        self.htmlp = htmlscript
+
+
+
+import seaborn as sns 
+import matplotlib.pyplot as plt
+
+class intro
+
