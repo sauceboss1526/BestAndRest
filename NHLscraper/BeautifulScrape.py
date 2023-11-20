02fa0bf768ae6648ca9b2d1e30333043ec39c8ce
sauceboss1526 <97584633+sauceboss1526@users.noreply.github.com>
2023-11-20 05:28:28 +0000

    operationalscraper

--- NHLscraper/BeautifulScrape.py
+++ NHLscraper/BeautifulScrape.py
@@ -9,7 +9,13 @@ nhlscraper: class method that uses BeautifulSoup to scrape nhl records website a
 import pandas as pd
 import numpy as np 
 from bs4 import BeautifulSoup 
+import urllib.request
 import csv 
 
 class intro:
-    def__init__(self, websiteurl)
+    def __init__(self, websiteurl):
+        self.htmldoc = urllib.request.urlopen(websiteurl)
+        html = self.htmldoc.read()
+        
+
+
